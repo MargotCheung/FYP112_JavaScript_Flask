f9d8244f56dd0ee29f4baa1f9ccb5037d504d935
suyc17 <judy102017@gmail.com>
2023-08-17 14:51:28 +0800

    整理code

--- Backend/app.py
+++ Backend/app.py
@@ -28,22 +28,8 @@ def learningProgress(user_id):
     return learningProgress_view(user_id)
 
 @app.route("/saveData", methods=["POST"])
-def save_data():
-    data = request.form  # 接收前端发送的JSON数据
-    print(data)
-    # save_data(data)  # 调用save_data()函数来保存数据
-    user_id="007"  
-    grade = data.get('grade')
-    course_name = data.get('course_name')
-    course_type = data.get('course_type')
-    course_credit = data.get('course_credit')
-    score = data.get('score')
-
-    sql_query = f'INSERT INTO user_grades (user_id, grade, course_name, course_type, course_credit, score) VALUES ("{user_id}","{grade}", "{course_name}","{course_type}","{course_credit}","{score}")'
-    # values = (grade, subject, category, credits, score)
-    cursor.execute(sql_query)
-    connection.commit()
-    return jsonify({"message": "Data saved successfully"})
+def saveData():
+    return save_data()
 
 @app.route("/CoinWallet")
 def CoinWallet():
--- backend/views/learningProgress.py
+++ backend/views/learningProgress.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, request, jsonify
-from backend.db import cursor
+from backend.db import cursor, connection
 
 
 def learningProgress_view(user_id):
@@ -9,11 +9,11 @@ def learningProgress_view(user_id):
     # print(credit[5])
     return render_template('learningProgress.html', **locals())
 
-def save_data(data):
+def save_data():
     # 接收通过AJAX发送的数据
-    # data = request.json 
-    # print(data)
-    # user_id = data.get('user_id')
+    data = request.form  # 接收前端发送的JSON数据
+    print(data)
+    # save_data(data)  # 调用save_data()函数来保存数据
     user_id="007"  
     grade = data.get('grade')
     course_name = data.get('course_name')
@@ -21,11 +21,10 @@ def save_data(data):
     course_credit = data.get('course_credit')
     score = data.get('score')
 
-    sql_query = f'INSERT INTO user_grades (user_id, grade, subject, category, credits, score) VALUES ("{user_id}","{grade}", "{course_name}","{course_type}","{course_credit}","{score}")'
+    sql_query = f'INSERT INTO user_grades (user_id, grade, course_name, course_type, course_credit, score) VALUES ("{user_id}","{grade}", "{course_name}","{course_type}","{course_credit}","{score}")'
     # values = (grade, subject, category, credits, score)
     cursor.execute(sql_query)
     connection.commit()
-
     # 返回响应
     return jsonify({'message': 'Grade submitted successfully'})
 
