08466aae393e964d8262abe3d2d27f2da1fd938f
mch <margot.cheung@gmail.com>
2023-08-27 18:15:58 +0800

    chartJS deployed

--- Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
+++ Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
@@ -1,21 +1,139 @@
-const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
+const ctx1 = document.getElementById('myChart1');
+const myChart1 = new Chart(ctx1, {
     type: 'radar',
     data: {
-      labels: ['數學', '編程', '創新', '解題', '邏輯'],
-      datasets: [{
-        label: '個人能力',
-        data: [1, 3, 3, 3, 5],
-        borderWidth: 1
-      }]
+        labels: ['數學', '編程', '創新', '解題', '邏輯'],
+        datasets: [{
+            label: '個人能力',
+            // data: [1, 3, 3, 3, 5],
+            data: [1, 2, 4, 3, 2],
+            borderWidth: 2
+        }]
     },
     options: {
         maintainAspectRatio: false, // Set to true to maintain aspect ratio, false to adjust independently
         scales: {
             y: {
                 beginAtZero: true
+            },
+            min: 0.0,
+            max: 5.0,
+            ticks: {
+                // forces step size to be 50 units
+                stepSize: 1.0
+            }
         }
-      }
     }
-  });
+});
+
+
+
+
+
+
+
+
+
+const ctx2 = document.getElementById('myChart2');
+const myChart2  = new Chart(ctx2, {
+    type: 'radar',
+    data: {
+        labels: ['數學', '編程', '創新', '解題', '邏輯'],
+          datasets: [{
+            label: 'My First Dataset',
+            // data: [65, 90, 81, 56, 40],
+            data: [1, 2, 4, 3, 2],
+            fill: true,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgb(255, 99, 132)',
+            pointBackgroundColor: 'rgb(255, 99, 132)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(255, 99, 132)'
+          }, {
+            label: 'My Second Dataset',
+            // data: [28, 48, 19, 96, 27],
+            data: [3, 3, 3, 4, 4],
+            fill: true,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            pointBackgroundColor: 'rgb(54, 162, 235)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(54, 162, 235)'
+          }]
+
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            },
+            min: 0.0,
+            max: 5.0,
+            ticks: {
+                // forces step size to be 50 units
+                stepSize: 1.0
+            }
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myChart2  = new Chart(ctx2, {
+//     type: 'radar',
+//     data: {
+//         labels: [
+//             'Eating',
+//             'Drinking',
+//             'Sleeping',
+//             'Designing',
+//             'Coding',
+//             'Cycling',
+//             'Running'
+//           ],
+//           datasets: [{
+//             label: 'My First Dataset',
+//             data: [65, 59, 90, 81, 56, 55, 40],
+//             fill: true,
+//             backgroundColor: 'rgba(255, 99, 132, 0.2)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             pointBackgroundColor: 'rgb(255, 99, 132)',
+//             pointBorderColor: '#fff',
+//             pointHoverBackgroundColor: '#fff',
+//             pointHoverBorderColor: 'rgb(255, 99, 132)'
+//           }, {
+//             label: 'My Second Dataset',
+//             data: [28, 48, 40, 19, 96, 27, 100],
+//             fill: true,
+//             backgroundColor: 'rgba(54, 162, 235, 0.2)',
+//             borderColor: 'rgb(54, 162, 235)',
+//             pointBackgroundColor: 'rgb(54, 162, 235)',
+//             pointBorderColor: '#fff',
+//             pointHoverBackgroundColor: '#fff',
+//             pointHoverBorderColor: 'rgb(54, 162, 235)'
+//           }]
+
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
+
+
+
