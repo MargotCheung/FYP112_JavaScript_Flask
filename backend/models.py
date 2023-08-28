5414e64ca08b0151f2cd6b1269c8c0ab92c60282
nicholas0714ong <nicholas0714ong@gmail.com>
2023-08-28 11:00:32 +0800

    models.py update

--- backend/models.py
+++ backend/models.py
@@ -13,3 +13,10 @@ class CourseInfoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     course_name = db.Column(db.String(100), nullable=False)
     teacher_name = db.Column(db.String(100), nullable=False)
+    course_id = db.Column(db.String(100), nullable=False)
+
+class CourseTecherModel(db.Model):
+    __tablename__ = "course_teacher"
+    index = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    course_name	= db.Column(db.String(10), nullable=False)
+    tc_name = db.Column(db.String(10), nullable=False)
