282260ace5eb4d94182a507cf5251a8da7dcde0b
nicholas0714ong <nicholas0714ong@gmail.com>
2023-09-01 01:13:22 +0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ SECRET_KEY = "89kjb98ycgw98"
 # 需要更改的就只有第6，7，8行的内容，分別是資料庫的名字，你的用戶名，你的密碼
 HOSTNAME    = '127.0.0.1'
 PORT        = '3306'
-DATABASE    = 'fengjia_db'
+DATABASE    = 'courselist'
 USERNAME    = 'flask'
 PASSWORD    = 'password'
 DB_URI      = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
