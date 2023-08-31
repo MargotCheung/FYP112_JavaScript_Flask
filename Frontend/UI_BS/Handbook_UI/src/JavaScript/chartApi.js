5bfe4f22f7eaa0e50d4d1e9f7f16b5d3e7daa043
mch <margot.cheung@gmail.com>
2023-09-01 01:14:22 +0800

    Update chartApi.js

--- Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
+++ Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
@@ -1,73 +1,37 @@
-// // -------------------------- myChart1 -------------------------------- //
-// const ctx1 = document.getElementById('myChart1');
-// const myChart1 = new Chart(ctx1, {
-//     type: 'radar',
-//     data: {
-//         labels: ['數學', '編程', '創新', '解題', '邏輯'],
-//         datasets: [{
-//             label: '個人能力',
-//             // data: [1, 3, 3, 3, 5],
-//             //data: [1, 2, 4, 3, 2],
-//             data: [0, 0, 0, 0, 0],
-//             borderWidth: 2
-//         }]
-//     },
-//     options: {
-//         maintainAspectRatio: false, // Set to true to maintain aspect ratio, false to adjust independently
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             },
-//             min: 0.0,
-//             max: 5.0,
-//             ticks: {
-//                 // forces step size to be 50 units
-//                 stepSize: 1.0
-//             }
-//         }
-//     }
-// });
-
-
+// -------------------------- myChart1 -------------------------------- //
 const ctx1 = document.getElementById('myChart1');
-const myChart1 = new Chart(ctx1, {
+const myChart1  = new Chart(ctx1, {
     type: 'radar',
     data: {
         labels: ['數學', '編程', '創新', '解題', '邏輯'],
-        datasets: [{
-            label: '個人能力',
-            data: [0, 0, 0, 0, 0],
-            borderWidth: 2
-        }]
+          datasets: [{            
+            label: '目前能力',
+            // data: [65, 90, 81, 56, 40],
+            data: [2, 3, 4, 2, 4],
+            fill: true,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            pointBackgroundColor: 'rgb(54, 162, 235)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(54, 162, 235)'
+          }]
+
     },
     options: {
-        maintainAspectRatio: false,
         scales: {
             y: {
-                beginAtZero: false, // Let the y-axis not always begin at 0 initially
-                suggestedMax: 5,
-                ticks: {
-                    stepSize: 1
-                }
-            }
-        },
-        plugins: {
-            afterLayout: ctx1 => {
-                // Manually set y-axis minimum to 0 if all data values are 0
-                const datasets = myChart1.data.datasets;
-                const allZero = datasets.every(dataset => dataset.data.every(value => value === 0));
-                if (allZero) {
-                    myChart1.scales.y.options.min = 0;
-                }
+                beginAtZero: true
+            },
+            min: 0.0,
+            max: 5.0,
+            ticks: {
+                // forces step size to be 50 units
+                stepSize: 1.0
             }
         }
     }
 });
-
-
-
-
-
 
 
 
@@ -79,9 +43,10 @@ const myChart2  = new Chart(ctx2, {
     data: {
         labels: ['數學', '編程', '創新', '解題', '邏輯'],
           datasets: [{
-            label: 'My First Dataset',
-            // data: [65, 90, 81, 56, 40],
-            data: [1, 2, 4, 3, 2],
+            label: '目前能力',
+            // data: [28, 48, 19, 96, 27],
+            data: [2, 3, 4, 2, 4],
+            //data: [3, 3, 4, 4, 4],
             fill: true,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgb(255, 99, 132)',
@@ -89,10 +54,10 @@ const myChart2  = new Chart(ctx2, {
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
             pointHoverBorderColor: 'rgb(255, 99, 132)'
-          }, {
-            label: 'My Second Dataset',
-            // data: [28, 48, 19, 96, 27],
-            data: [3, 3, 3, 4, 4],
+          }, {            
+            label: '原始能力',
+            // data: [65, 90, 81, 56, 40],
+            data: [1, 2, 4, 3, 2],
             fill: true,
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgb(54, 162, 235)',
