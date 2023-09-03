bdaa54fe690fc69c83f4aef38d2e3f4d20d1cee8
mch <margot.cheung@gmail.com>
2023-09-03 18:06:58 +0800

    charts fixed - can begin at zero

--- Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
+++ Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
@@ -1,89 +1,99 @@
 // -------------------------- myChart1 -------------------------------- //
 const ctx1 = document.getElementById('myChart1');
-const myChart1  = new Chart(ctx1, {
+const myChart1 = new Chart(ctx1, {  
     type: 'radar',
     data: {
         labels: ['數學', '編程', '創新', '解題', '邏輯'],
-          datasets: [{            
-            label: '目前能力',
-            // data: [65, 90, 81, 56, 40],
+        datasets: [{
+            label: '目前能力值', 
             data: [2, 3, 4, 2, 4],
-            fill: true,
+            borderWidth: 2,
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgb(54, 162, 235)',
             pointBackgroundColor: 'rgb(54, 162, 235)',
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
             pointHoverBorderColor: 'rgb(54, 162, 235)'
-          }]
-
+        }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            },
-            min: 0.0,
-            max: 5.0,
+        scale: {
             ticks: {
-                // forces step size to be 50 units
-                stepSize: 1.0
+                beginAtZero: false, // 刻度从0开始
+                stepSize: 1,       // 刻度间距
+                max: 5,            // 刻度的最大值
+                min: 0             // 刻度的最小值
             }
         }
     }
 });
+
+
+
+
+// -------------------------- polarArea myChart1 -------------------------------- //
+// const ctx1 = document.getElementById('myChart1');
+// const myChart1  = new Chart(ctx1, {
+//     type: 'polarArea',
+//     data: {
+//         labels: ['數學', '編程', '創新', '解題', '邏輯'],
+//           datasets: [{            
+//             // data: [65, 90, 81, 56, 40],
+//             data: [2, 3, 4, 2, 4],
+//             fill: true,
+//             backgroundColor: [
+//                 'rgb(255, 99, 132)',
+//                 'rgb(75, 192, 192)',
+//                 'rgb(255, 205, 86)',
+//                 'rgb(201, 203, 207)',
+//                 'rgb(54, 162, 235)'
+//             ]
+//           }]    
+
+//     },
+// });
+
 
 
 
 
 // -------------------------- myChart2 -------------------------------- //
 const ctx2 = document.getElementById('myChart2');
-const myChart2  = new Chart(ctx2, {
+const myChart2 = new Chart(ctx2, {  
     type: 'radar',
     data: {
         labels: ['數學', '編程', '創新', '解題', '邏輯'],
-          datasets: [{
-            label: '目前能力',
-            // data: [28, 48, 19, 96, 27],
+        datasets: [{
+            label: '目前能力值', 
             data: [2, 3, 4, 2, 4],
-            //data: [3, 3, 4, 4, 4],
-            fill: true,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgb(255, 99, 132)',
-            pointBackgroundColor: 'rgb(255, 99, 132)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(255, 99, 132)'
-          }, {            
-            label: '原始能力',
-            // data: [65, 90, 81, 56, 40],
-            data: [1, 2, 4, 3, 2],
-            fill: true,
+            borderWidth: 2,
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgb(54, 162, 235)',
             pointBackgroundColor: 'rgb(54, 162, 235)',
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
             pointHoverBorderColor: 'rgb(54, 162, 235)'
-          }]
-
+        }, {
+            label: '原始能力值',
+            data: [1, 2, 4, 3, 2],
+            borderWidth: 2,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgb(255, 99, 132)',
+            pointBackgroundColor: 'rgb(255, 99, 132)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(255, 99, 132)'
+        }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            },
-            min: 0.0,
-            max: 5.0,
+        scale: {
             ticks: {
-                // forces step size to be 50 units
-                stepSize: 1.0
+                beginAtZero: false, // 刻度从0开始
+                stepSize: 1,       // 刻度间距
+                max: 5,            // 刻度的最大值
+                min: 0             // 刻度的最小值
             }
         }
     }
 });
-
-
-
-
 
