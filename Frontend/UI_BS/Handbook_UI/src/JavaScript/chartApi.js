f0e0df6bc08a64da7b19f0d0f218836273c59051
mch <margot.cheung@gmail.com>
2023-09-18 22:58:34 +0800

    commit chart

--- Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
+++ Frontend/UI_BS/Handbook_UI/src/JavaScript/chartApi.js
@@ -23,12 +23,6 @@ const myChart1 = new Chart(ctx1, {
 
 
 
-
-
-
-
-
-
 const ctx2 = document.getElementById('myChart2');
 const myChart2  = new Chart(ctx2, {
     type: 'radar',
@@ -76,59 +70,43 @@ const myChart2  = new Chart(ctx2, {
 
 
 
+const ctx3 = document.getElementById('myChart3');
+const myChart3 = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+});
 
-
-
-
-
-
-
-
-
-
-// const myChart2  = new Chart(ctx2, {
-//     type: 'radar',
-//     data: {
-//         labels: [
-//             'Eating',
-//             'Drinking',
-//             'Sleeping',
-//             'Designing',
-//             'Coding',
-//             'Cycling',
-//             'Running'
-//           ],
-//           datasets: [{
-//             label: 'My First Dataset',
-//             data: [65, 59, 90, 81, 56, 55, 40],
-//             fill: true,
-//             backgroundColor: 'rgba(255, 99, 132, 0.2)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             pointBackgroundColor: 'rgb(255, 99, 132)',
-//             pointBorderColor: '#fff',
-//             pointHoverBackgroundColor: '#fff',
-//             pointHoverBorderColor: 'rgb(255, 99, 132)'
-//           }, {
-//             label: 'My Second Dataset',
-//             data: [28, 48, 40, 19, 96, 27, 100],
-//             fill: true,
-//             backgroundColor: 'rgba(54, 162, 235, 0.2)',
-//             borderColor: 'rgb(54, 162, 235)',
-//             pointBackgroundColor: 'rgb(54, 162, 235)',
-//             pointBorderColor: '#fff',
-//             pointHoverBackgroundColor: '#fff',
-//             pointHoverBorderColor: 'rgb(54, 162, 235)'
-//           }]
-
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// });
-
-
-
+const ctx = document.getElementById('myChart');
+  
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
--- backend/static/js/chartApi.js
+++ backend/static/js/chartApi.js
@@ -28,6 +28,28 @@ const myChart1 = new Chart(ctx1, {
 
 
 
+const ctx3 = document.getElementById('myChart3');
+const myChart3 = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+});
+
+
+
 
 
 
