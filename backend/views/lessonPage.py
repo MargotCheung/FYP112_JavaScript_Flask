03ee3e228196449b335b9fd7ff55a531f18a6314
suyc17 <judy102017@gmail.com>
2023-09-23 21:05:37 +0800

    推薦課程寫了大概
    還差細節的推薦配分，code的部分完成了

--- backend/views/lessonPage.py
+++ backend/views/lessonPage.py
@@ -1,6 +1,8 @@
 from flask import render_template, url_for, request, jsonify, abort,g
 from backend.db import cursor
-
+from ..models import UserGradeModel, CourseInfoModel, UserModel
+from ..extends import db
+from ..blueprints.form import MyResultSearchForm
 
 def lessonPage_view():
     user_id = g.user.id
@@ -35,6 +37,10 @@ def lessonPage_view():
 # ]
 
 def get_lesson_name():
+    user_id = g.user.id
+    # 得到user的全部内容，後續都會用到
+    user = UserModel.query.filter_by(id=user_id).first()
+
     amount = request.args.get('amount', None)
     page = request.args.get('page', None)
     if amount is None and page is None:
@@ -43,28 +49,43 @@ def get_lesson_name():
     page = int(page)
     sql_query = 'SELECT * FROM course_info;'
     count = cursor.execute(sql_query)
-    course_names = [{
-        # 'course_id': row[0],
-        # 'course_year': row[1],
-        # 'course_name': row[2],
-        # 'course_type': row[3],
-        # 'course_credit': row[4],
-        # 'course_intro': row[5],
-        # 'math': row[6],
-        # 'coding': row[7],
-        # 'logic': row[8],
-        # 'creative': row[9],
-        # 'slove': row[10],
-        'course_name': row[0],
-        'course_year': row[1],
-        'course_type': row[2],
-        'course_credit': row[3],
-        'course_intro': row[4],
-        'math': row[5],
-        'coding': row[6],
-        'logic': row[7],
-        'creative': row[8],
-        'slove': row[9],
-    } for row in cursor.fetchall()]
+    course_names = []
+    for row in cursor.fetchall():
+        course = {
+            'course_name': row[0],
+            'course_year': row[1],
+            'course_type': row[2],
+            'course_credit': row[3],
+            'course_intro': row[4],
+            'math': row[5],
+            'coding': row[6],
+            'logic': row[7],
+            'creative': row[8],
+            'slove': row[9],
+        }
+        temp=0
+        if user.ability_math >= row[5]:
+            temp+=1
+        if user.ability_coding >= row[6]:
+            temp+=1
+        if user.ability_logic >= row[7]:
+            temp+=1
+        if user.ability_creative >= row[8]:
+            temp+=1
+        if user.ability_solve >= row[9]:
+            temp+=1
+        # # 计算 ability，这里假设将 math、coding、logic、creative、slove 相加
+        # ability = row[5] + row[6] + row[7] + row[8] + row[9]
+        if temp>3:
+            ability="綽綽有餘"
+        elif temp==3:
+            ability="還可以"
+        elif temp==2:
+            ability="有點勉強"
+        elif temp<2:
+            ability="不太推薦"
+        course['ability'] = ability  # 将计算结果添加到字典中
+        course_names.append(course)  # 将字典添加到列表中
+
     query_course_names = course_names[amount*page:amount*(page+1)]
     return jsonify(query_course_names=query_course_names)
